07e094e17e6c39d9e6ebdbe25aab23cb2b4581d1
Diarmuid Murphy <diarmuid.com@gmail.com>
2014-10-17 19:55:13 -0700

    project euler 2 finished

--- project_euler_2.rb
+++ project_euler_2.rb
@@ -2,6 +2,7 @@ previous_number = 1
 current_number = 2
 
 sequence = [previous_number, current_number]
+even_sequence = []
 
 max_number = 4000000
 
@@ -12,7 +13,12 @@ while current_number < max_number do
 end
 
 sequence.pop
-
 #puts sequence
 
-puts sequence.reduce(:+)
+sequence.each do |item|
+	if item % 2 == 0
+		even_sequence.push(item)
+	end
+end
+
+puts even_sequence.reduce(:+)
